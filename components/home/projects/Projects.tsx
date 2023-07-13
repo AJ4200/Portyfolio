2ced0b9af3dea222dd7de4dc19e3288e927801cd
Abel Majadibodu <Abeljackson33@gmail.com>
2023-07-13 15:20:14 +0200

    😭😭😭

--- components/home/projects/Projects.tsx
+++ components/home/projects/Projects.tsx
@@ -1,30 +1,41 @@
+import { useState, useEffect } from "react";
 import { SectionHeader } from "@/components/utils/SectionHeader";
 import { Project } from "./Project";
-import styles from "./projects.module.scss";
-import projectsData from "public/projectsData.json";
-import {Carousel} from "react-responsive-carousel";
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
  export const Projects = () => {
-  return (
+  const [data, setData] = useState([]);
+   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/projectsData.json");
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+     fetchData();
+  }, []);
+   return (
     <section className="section-wrapper" id="projects">
       <SectionHeader title="Projects" dir="r" />
       <Carousel autoPlay infiniteLoop stopOnHover>
-      {projectsData.map((project) => {
-          return (
-            <Project
-              key={project.title}
-              title={project.title}
-              imgSrc={project.imgSrc}
-              code={project.code}
-              projectLink={project.projectLink}
-              tech={project.tech}
-              description={project.description}
-              modalContent={<p dangerouslySetInnerHTML={{ __html: project.modalContent }} />}
-            />
-          );
-        })}
-            </Carousel>
+        {data.map((project:any) => (
+          <Project
+            key={project.title}
+            title={project.title}
+            imgSrc={project.imgSrc}
+            code={project.code}
+            projectLink={project.projectLink}
+            tech={project.tech}
+            description={project.description}
+            modalContent={
+              <p dangerouslySetInnerHTML={{ __html: project.modalContent }} />
+            }
+          />
+        ))}
+      </Carousel>
     </section>
   );
 };

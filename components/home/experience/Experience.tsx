aa2007191936ebc4ab12ecf59b7ae44b85760a33
Abel Majadibodu <Abeljackson33@gmail.com>
2023-07-18 15:31:30 +0200

    updated experience to real data

--- components/home/experience/Experience.tsx
+++ components/home/experience/Experience.tsx
@@ -19,7 +19,7 @@ const experience = [
     time: "2022 - Present",
     location: "Remote",
     description:
-      "Full Stack developer. I created, manage an scale jep product such as Portyfolio(this site), CommonFunLib and CodeShifter from scratch using the  most effecient tech stacks",
+      "Full Stack developer. I create, manage and scale jep products such as Portyfolio(this site), H.D.F.Player, CommonFunLib and more from scratch using the most effecient tech stacks",
     tech: [
       "Nodejs",
       "Python",
@@ -30,15 +30,25 @@ const experience = [
       "Maven",
       "MongoDB",
       "PostgreSQL",
+      "and more"
     ],
   },
   {
-    title: "Capdatacon.io",
-    position: "Data Analyst Intern",
-    time: "2020 Feb - 2020 July",
-    location: "Remote",
+    title: "UJ Maqhawe's Men Residence",
+    position: "IT Admin",
+    time: "2020 Feb - 2022 Dec",
+    location: "SA, Johannesburg",
     description:
-      "Junior Data Analyst was responsible to represent data in all data formats(e.g JSON, CSV and etc) using JSONCSV",
-    tech: ["pgAdmin", "R", "Python", "GraphQL", "Azure", "JSON", "CSV", "Excel"],
+      "I ensured the smooth operation of servers, routers, and Wi-Fi systems through regular checks and maintenance. Additionally, I promptly addressed any issues and resolved problems to minimize downtime. I also monitored network performance, reporting any outages or connectivity issues to the relevant teams",
+    tech: ["Netgear Admin", "Excel", "Hands"],
+  },
+  {
+    title: "UJ Maqhawe's Men Residence",
+    position: "Web Developer",
+    time: "2023 Feb - 2023 Dec",
+    location: "SA, Johannesburg",
+    description:
+      "I designed and built a user-friendly web interface that resulted in increased user adoption and engagement. The intuitive and accessible design improved user experience and led to higher submission rates. Additionally, the implementation of secure authentication and authorization mechanisms ensured authorized access for students, and an efficient issue tracking and management system streamlined resolution and improved response times.",
+    tech: ["Excel", "MS Access", "ASPN.NET", "JavaFx"],
   },
 ];
